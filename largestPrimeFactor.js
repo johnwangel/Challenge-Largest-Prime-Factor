b27efe43f64e4d8818019ca3914ed79d4a2bc466
John Atkins <johnatkins1999@yahoo.com>
2017-06-21 10:31:36 -1000

    finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,16 @@
+/*jshint esversion: 6 */
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
 
-  return primeNumber;
+  function isPrime(testNo){
+    for (let j = 2; j <= Math.floor(Math.sqrt(testNo)); j++) {
+      if (j == testNo){ return testNo; }
+      if (testNo%j === 0){ return false; }
+    }
+  }
+
+  for (let i = Math.floor(Math.sqrt(n)); i > 0 ; i--) {
+    if (n%i === 0){
+      if (isPrime(i) !== false) { return i; }
+    }
+  }
 };
